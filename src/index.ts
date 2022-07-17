8f7dc1646c08bb2dde5f957f57ef04520a1aed9f
jekvosm <yn4k-jek@yandex.ru>
2022-07-17 23:56:13 +0400

    сreated a web framework

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { UserEdit } from './views/UserEdit'
+import { User } from './models/User'
+
+const user = User.buildUser({ name: 'NAME', age: 20 })
+
+const root = document.getElementById('root')
+
+if (root) {
+  new UserEdit(root, user).render()
+} else {
+  throw Error('Root element not found')
+}
+
+// import { UserList } from './views/UserList'
+// import { Collection } from './models/Collection'
+// import { User, UserProps } from './models/User'
+
+// const users = new Collection(
+//   'http://localhost:3000/users',
+//   (json: UserProps) => {
+//     return User.buildUser(json)
+//   }
+// )
+
+// users.on('change', () => {
+//   const root = document.getElementById('root')
+
+//   if (root) {
+//     new UserList(root, users).render()
+//   }
+// })
+
+// users.fetch()
